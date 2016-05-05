e00501957a14e3cc1ae21891222410cc97bd45d8
chenhui <1114545064@qq.com>
2016-05-05 22:55:59 +0800

    all

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,3 +1,21 @@
 ;(function($){
-    
+    $('.zj_left').on('mouseover','li',function(e){
+        e.stopPropagation();
+        var $index = $(this).index();
+        $(this).addClass('on').siblings().removeClass('on');
+        $('.zj_center').find('ul').eq($index).show().siblings().hide();
+    })    
+
+    $('.detail_left_ul').on('click','li',function(e){
+        e.stopPropagation();
+        var $this = $(this);
+        if($this.find('.first').hasClass('on')){
+            $this.find('.first').removeClass('on')
+            $this.children('ul').hide();
+            $this.siblings().find('first').removeClass('on')
+            return;
+        }
+        $this.find('ul').show().parent().siblings().find('ul').hide();
+        $this.find('.first').addClass('on').parent().siblings().find('.first').removeClass('on');
+    })
 })(jQuery)
